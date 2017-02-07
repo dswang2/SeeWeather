b1c2b689b239cfb7da0cb66d71d42fb106a2dcec
ame <465616185@qq.com>
2017-02-07 17:40:52 +0800

    overridePendingTransition study

--- app/src/main/java/com/xiecc/seeWeather/modules/launch/FirstActivity.java
+++ app/src/main/java/com/xiecc/seeWeather/modules/launch/FirstActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+
+import com.xiecc.seeWeather.R;
 import com.xiecc.seeWeather.modules.main.ui.MainActivity;
 import java.lang.ref.WeakReference;
 
@@ -19,7 +21,15 @@ public class FirstActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //activity切换的淡入淡出效果
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        //overridePendingTransition 这个函数有两个参数,一个参数是第一个activity进入时的动画,另外一个参数则是第二个activity退出时的动画
+        //android.R.anim.fade_in, android.R.anim.fade_out 是Android系统的淡入淡出效果
+        //参考：http://blog.csdn.net/qq_30379689/article/details/52494270
+        //overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        //自定义的activity淡入淡出效果，动画文件在 res - anim 目录下
+        overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
+
         super.onCreate(savedInstanceState);
         mHandler.sendEmptyMessageDelayed(1, 1000);
 
